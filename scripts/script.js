f0334bc0ac81808df2e1200d6d3690cf9cebe9f9
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-03-04 21:22:54 +0100

    added open add contact overlay

--- scripts/script.js
+++ scripts/script.js
@@ -4,3 +4,16 @@ function toggleDropdown() {
     let dropdownREF = document.getElementById("dropdown");
     dropdownREF.classList.toggle("d-none");
 }
+
+
+// CONTACTS -----------------------------------
+/**
+ * Used to open an overlay menu.
+ * 
+ * @param {string} containerId - This is the selected container menu.
+ * @param {string} overlayClass - Adds the specified class to the container to open the menu.
+ */
+function toggleOverlayMenu(containerId, overlayClass){
+    let container = document.getElementById(containerId);
+        container.classList.toggle(overlayClass);
+}
