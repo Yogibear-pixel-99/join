9e0b7b481a430dbce1705a403959d38e8dbdd390
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-03-05 18:38:03 +0100

    change overlay function

--- scripts/script.js
+++ scripts/script.js
@@ -12,19 +12,19 @@ function toggleDropdown() {
  * Used to open an overlay menu.
  * 
  * @param {string} containerId - This is the selected container menu.
- * @param {string} overlayClass - Adds the specified class to the container to open the menu.
+ * @param {string} className - Adds the specified class to the container to open the menu.
  */
-function toggleOverlayMenu(containerId, overlayClass){
-    let container = document.getElementById(containerId);
-        container.classList.toggle(overlayClass);
+function toggleOverlayMenu(containerId, className){
+    let overlay = document.getElementById(containerId);
+        overlay.classList.toggle(className);
 }
-
-
 
 function toggleAddTaskOverlay() {
     let addtaskREF = document.getElementById("addtask-overlay");
     addtaskREF.classList.toggle("d-none")
 }
+
+
 
 
 function noClose(event) {
