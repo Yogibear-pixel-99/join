a7a2507b5bdf0aadafa750491f1b4d06c1050b8b
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-03-10 21:59:26 +0100

    started creating new user sign up function

---
+++ scripts/signUp.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+// get infos from: value
+
+// name
+// email
+// password
+// confirm password
+
+
+
+// function who checks if 
+
+// password and confirm is same
+// privacy is checked
+// user allready exists
+// email allready exists
+
+
+// then update data to firebase
+
+// box with: "you signed up successfully"
+
+// directed to log in page
+
+
+
+function signUpNewUser(){
+    const name = document.getElementById('sign-up-name').value;
+    const email = document.getElementById('sign-up-email').value;
+    const password = document.getElementById('sign-up-password').value;
+    const passwordConfirm = document.getElementById('sign-up-password-confirm').value;
+    const privacyCheck = document.getElementById('accept-privacy');
+        if (checkValuesFromSignUpFields(name, email, password, passwordConfirm, privacyCheck)) {
+            fetchNewUserToDatabase(name, email, password, passwordConfirm, privacyCheck);
+        } 
+        
+
+}
+
+
+function checkValuesFromSignUpFields(name, email, password, passwordConfirm, privacyCheck){
+    if (name != '' && email != '' && (password === passwordConfirm) && privacyCheck.checked) {
+        return true;
+    }
+}
+
+
+function fetchNewUserToDatabase(name, email, password, passwordConfirm, privacyCheck){
+    
+}
