383e86cc5ebfe025e09263ee062916e58124da55
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-03-11 10:52:33 +0100

    testing collect form data for sign up

--- scripts/signUp.js
+++ scripts/signUp.js
@@ -26,29 +26,47 @@
 
 // directed to log in page
 
+let formInputTest = [];
+
+function signUpNewUser() {
+    let newUserData = {};
+    // if (checkPasswordAndConfirmPasswordIsSame()) {
+        
+    // }
+    // ;
+
+    let formInfos = document.getElementById('sign-up-form');
+    let data = new FormData(formInfos);
+
+    newUserData = Object.fromEntries(data);
 
 
-function signUpNewUser(){
-    const name = document.getElementById('sign-up-name').value;
-    const email = document.getElementById('sign-up-email').value;
-    const password = document.getElementById('sign-up-password').value;
-    const passwordConfirm = document.getElementById('sign-up-password-confirm').value;
-    const privacyCheck = document.getElementById('accept-privacy');
-        if (checkValuesFromSignUpFields(name, email, password, passwordConfirm, privacyCheck)) {
-            fetchNewUserToDatabase(name, email, password, passwordConfirm, privacyCheck);
-        } 
+    console.log(formInputTest);
+}
+
+
+
+// function signUpNewUser(){
+//     const name = document.getElementById('sign-up-name').value;
+//     const email = document.getElementById('sign-up-email').value;
+
+//     const privacyCheck = document.getElementById('accept-privacy');
+//         if (checkValuesFromSignUpFields(name, email, password, passwordConfirm, privacyCheck)) {
+//             fetchNewUserToDatabase(name, email, password, passwordConfirm, privacyCheck);
+//         } 
         
 
-}
+// }
 
 
-function checkValuesFromSignUpFields(name, email, password, passwordConfirm, privacyCheck){
-    if (name != '' && email != '' && (password === passwordConfirm) && privacyCheck.checked) {
-        return true;
-    }
-}
+// function checkValuesFromSignUpFields(name, email, password, passwordConfirm, privacyCheck){
+//     if (name != '' && email != '' && password != '' && (password === passwordConfirm) && privacyCheck.checked) {
+//         return true;
+//     }
+// }
 
 
-function fetchNewUserToDatabase(name, email, password, passwordConfirm, privacyCheck){
-    console.log('ready to fetch');
-}
+// function fetchNewUserToDatabase(name, email, password, passwordConfirm, privacyCheck){
+// function fetchNewUserToDatabase(){
+//     console.log('ready to fetch');
+// }
