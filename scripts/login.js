7722d5a0804f397f3f6383f8a6ff5c1c6f727a03
EmreIscan-47 <iscan.connect@outlook.de>
2025-03-14 15:20:36 +0100

    Some Login Function code added

--- scripts/login.js
+++ scripts/login.js
@@ -1,13 +1,48 @@
-/* const emailREF = document.getElementById("email").value;
-const passwordREF = document.getElementById("password").value;
+/* 
 const loginREF = document.getElementById("login"); */
+let emailUserList = [];
+let passwordUserList = [];
 
 async function getUserInfo() {
-   await getDataFromServer('users', usersFromApi);
+  await getDataFromServer("users", usersFromApi);
+  getAllEmail();
+  getAllPasswords();
+  
+  
 }
 
-function checkLogin() {
+function getAllEmail() {
+    for (let index = 0; index < usersFromApi.length; index++) {
+       emailUserList.push(usersFromApi[index].email); 
+    }
+    console.log(emailUserList);
+    
+    let position = emailUserList.indexOf('lukas@schmidt.com')
+    console.log(position);
     
 }
 
+function getAllPasswords() {
+    for (let index = 0; index < usersFromApi.length; index++) {
+        passwordUserList.push(usersFromApi[index].password); 
+     }
+     
+}
 
+
+
+function checkLogin() {
+  const passwordREF = document.getElementById("password").value;
+  checkEmail();
+}
+
+function checkEmail() {
+  const emailREF = document.getElementById("email").value;
+ 
+}
+
+function checkPassword() {
+  const passwordREF = document.getElementById("password").value;
+}
+
+getUserInfo();
