6a484a640b7f651792daeccde72e1809f05ea399
EmreIscan-47 <iscan.connect@outlook.de>
2025-03-14 03:33:36 +0100

    Login function added

--- scripts/login.js
+++ scripts/login.js
@@ -1,2 +1,13 @@
-const emailREF = document.getElementById("email").value;
+/* const emailREF = document.getElementById("email").value;
 const passwordREF = document.getElementById("password").value;
+const loginREF = document.getElementById("login"); */
+
+async function getUserInfo() {
+   await getDataFromServer('users', usersFromApi);
+}
+
+function checkLogin() {
+    
+}
+
+
