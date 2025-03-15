20b774c0e7e165306e075266ec33a6c85b9d22fd
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-03-15 13:28:58 +0100

    added subtasks icons function onFocus

--- scripts/addtask.js
+++ scripts/addtask.js
@@ -1,4 +1,10 @@
 
+
+function resetForm(formId){
+  const ref = document.getElementById(formId);
+        ref.reset();
+}
+
 function setPriorityButtonColor(selected) {
   const labelRef = document.querySelectorAll("#priority-wrapper label");
   labelRef.forEach((element) => {
@@ -10,4 +16,18 @@ function setPriorityButtonColor(selected) {
       element.querySelector('svg').classList.remove('prio-svg');
     }
   });
+}
+
+function showSubtasksInputMenu(){
+  const plusIcon = document.getElementById('subtasks-plus');
+  const focusIcons = document.getElementById('subtasks-on-focus-icons');
+        plusIcon.classList.add('d-none');
+        focusIcons.classList.remove('d-none');
+}
+
+function hideSubtasksInputMenu(){
+  const plusIcon = document.getElementById('subtasks-plus');
+  const focusIcons = document.getElementById('subtasks-on-focus-icons');
+        plusIcon.classList.remove('d-none');
+        focusIcons.classList.add('d-none');
 }
