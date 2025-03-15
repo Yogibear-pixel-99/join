441914627ec4f46741d811c9412f8a9ffd79da41
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-03-15 11:49:46 +0100

    added symbol color to radio buttons

--- scripts/addtask.js
+++ scripts/addtask.js
@@ -40,8 +40,10 @@ function setPriorityButtonColor(selected) {
   labelRef.forEach((element) => {
     if (element === selected) {
       element.classList.add("active");
+      element.querySelector('svg').classList.add('prio-svg');
     } else {
       element.classList.remove("active");
+      element.querySelector('svg').classList.remove('prio-svg');
     }
   });
 }
