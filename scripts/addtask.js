ed429f2d68bf484eeb73c1f57ce1a2207e1c8a7c
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-03-15 11:44:58 +0100

    changed radio buttons prio

--- scripts/addtask.js
+++ scripts/addtask.js
@@ -1,36 +1,47 @@
 function setActive(button, priority) {
-    if (button.classList.contains('active')) {
-      button.classList.remove('active');
-      button.querySelector('img').classList.remove('active');
-      button.style.backgroundColor = '#ffffff'; 
-      button.querySelector('img').style.filter = ''; 
-      return;
-    }
-   
-    const buttons = document.querySelectorAll('.btn-importance');
-    buttons.forEach(btn => {
-      btn.classList.remove('active');
-      btn.querySelector('img').classList.remove('active');
-      btn.style.backgroundColor = '#ffffff'; 
-      btn.querySelector('img').style.filter = ''; 
-    });
-  
-    button.classList.add('active');
-    button.querySelector('img').classList.add('active');
-    
-    if (priority === 'prio') {
-      button.style.backgroundColor = 'red'; 
-    } else if (priority === 'medium') {
-      button.style.backgroundColor = '#FFA800'; 
-    } else if (priority === 'low') {
-      button.style.backgroundColor = 'green'; 
-    }
-  
-    button.querySelector('img').style.filter = 'brightness(0) saturate(100%) invert(1)';
+  if (button.classList.contains("active")) {
+    button.classList.remove("active");
+    button.querySelector("img").classList.remove("active");
+    button.style.backgroundColor = "#ffffff";
+    button.querySelector("img").style.filter = "";
+    return;
   }
-  
-  
-  function resetForm(formId){
-    const ref = document.getElementById(formId);
-          ref.reset();
+
+  const buttons = document.querySelectorAll(".btn-importance");
+  buttons.forEach((btn) => {
+    btn.classList.remove("active");
+    btn.querySelector("img").classList.remove("active");
+    btn.style.backgroundColor = "#ffffff";
+    btn.querySelector("img").style.filter = "";
+  });
+
+  button.classList.add("active");
+  button.querySelector("img").classList.add("active");
+
+  if (priority === "prio") {
+    button.style.backgroundColor = "red";
+  } else if (priority === "medium") {
+    button.style.backgroundColor = "#FFA800";
+  } else if (priority === "low") {
+    button.style.backgroundColor = "green";
   }
+
+  button.querySelector("img").style.filter =
+    "brightness(0) saturate(100%) invert(1)";
+}
+
+function resetForm(formId) {
+  const ref = document.getElementById(formId);
+  ref.reset();
+}
+
+function setPriorityButtonColor(selected) {
+  const labelRef = document.querySelectorAll("#priority-wrapper label");
+  labelRef.forEach((element) => {
+    if (element === selected) {
+      element.classList.add("active");
+    } else {
+      element.classList.remove("active");
+    }
+  });
+}
