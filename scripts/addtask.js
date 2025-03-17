947943f9bd2a41042cb55cbccc6fa941b3dd024e
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-03-17 12:32:16 +0100

    added subtask template

--- scripts/addtask.js
+++ scripts/addtask.js
@@ -1,8 +1,6 @@
-
-
-function resetForm(formId){
+function resetForm(formId) {
   const ref = document.getElementById(formId);
-        ref.reset();
+  ref.reset();
 }
 
 function setPriorityButtonColor(selected) {
@@ -10,63 +8,73 @@ function setPriorityButtonColor(selected) {
   labelRef.forEach((element) => {
     if (element === selected) {
       element.classList.add("active");
-      element.querySelector('svg').classList.add('prio-svg');
+      element.querySelector("svg").classList.add("prio-svg");
     } else {
       element.classList.remove("active");
-      element.querySelector('svg').classList.remove('prio-svg');
+      element.querySelector("svg").classList.remove("prio-svg");
     }
   });
 }
 
-function showSubtasksInputMenu(){
-  const plusIcon = document.getElementById('subtasks-plus');
-  const focusIcons = document.getElementById('subtasks-on-focus-icons');
-        plusIcon.classList.add('d-none');
-        focusIcons.classList.remove('d-none');
+function showSubtasksInputMenu() {
+  const plusIcon = document.getElementById("subtasks-plus");
+  const focusIcons = document.getElementById("subtasks-on-focus-icons");
+  plusIcon.classList.add("d-none");
+  focusIcons.classList.remove("d-none");
 }
 
-function delayHideSubtasksInputMenu(){
-  setTimeout(() => hideSubtasksInputMenu(), 100);
+function delayHideSubtasksInputMenu() {
+  setTimeout(() => hideSubtasksInputMenu(), 150);
 }
 
-function hideSubtasksInputMenu(){
-  const plusIcon = document.getElementById('subtasks-plus');
-  const focusIcons = document.getElementById('subtasks-on-focus-icons');
-        plusIcon.classList.remove('d-none');
-        focusIcons.classList.add('d-none');
+function hideSubtasksInputMenu() {
+  const plusIcon = document.getElementById("subtasks-plus");
+  const focusIcons = document.getElementById("subtasks-on-focus-icons");
+  plusIcon.classList.remove("d-none");
+  focusIcons.classList.add("d-none");
 }
 
 let allSubtasks = [];
 
-function addSubtaskInputInForm(){
-    
-    let subtaskInput = document.getElementById('subtasks-input');
-        allSubtasks.push(subtaskInput.value);
-        renderSubtasksToForm();
-        subtaskInput.value = '';
-          
-        
+function addSubtaskValueToArray() {
+  let subtaskInput = document.getElementById("subtasks-input");
+  allSubtasks.push(subtaskInput.value);
+  renderSubtasksFromArrayToForm();
+  subtaskInput.value = "";
 }
 
-function renderSubtasksToForm(){
-  let subtaskContent = document.getElementById('added-subtasks');
-      subtaskContent.innerHTML = '';
-      for (let subtaskIndex = 0; subtaskIndex < allSubtasks.length; subtaskIndex++) {
-        const element = allSubtasks[subtaskIndex];
-        subtaskContent.innerHTML += getSubtaskToFormTemp(element);
-      }
-      
+function renderSubtasksFromArrayToForm() {
+  let subtaskContent = document.getElementById("added-subtasks");
+  subtaskContent.innerHTML = "";
+  for (
+    let subtaskIndex = 0;
+    subtaskIndex < allSubtasks.length;
+    subtaskIndex++
+  ) {
+    const element = allSubtasks[subtaskIndex];
+    subtaskContent.innerHTML += getSubtaskToFormTemp(element, subtaskIndex);
+  }
 }
 
-function getSubtaskToFormTemp(subtask){
-  return `<div>${subtask}</div>`
+function getSubtaskToFormTemp(subtask, index) {
+  return `<div class="subtask-single-wrapper" >
+            <span>&#8226</span>
+            <input 
+              type="text" 
+              name="subtask-${index}" 
+              value="${subtask}">
+            <div class="single-task-icon-wrapper">
+              
+            </div>
+          </div>`;
 }
+
 // collect info from subtask field
 // put the info to a container below
-  // get a unorderd list template
-    // the template should have an edit and erase symbol
-    // on edit i should be able to edit the information
-    // on erase the info should delete
+// get a unorderd list template
+// the template should have an edit and erase symbol
+// on edit i should be able to edit the information
+// on erase the info should delete
 // on hover there should be a grey background
 // only on hover the symbols for editing and delete should appear
 
