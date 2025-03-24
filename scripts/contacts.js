e342666490dcb618ea1ac3762d2e5c346c17d9cf
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-03-24 19:25:52 +0100

    added documentation to contacts functions

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -1,257 +1,336 @@
 
-
-
 let sortedContactsArrayByFirstLetter = [];
-
-
-
 
 /**
  * Get contacts from API firebase, create initials from first- and lastname, sort contacts by first letter and render the contacst header letter to HTML.
- * 
+ *
  */
-async function sortAndRenderContacts(){
-    await getDataFromServer('contacts', contactsFromApi);
-    createInitialsForEachName(contactsFromApi);  
-    sortAllContactsByFirstLetter();
-    renderContactsHeaderLetter();
+async function sortAndRenderContacts() {
+  await getDataFromServer("contacts", contactsFromApi);
+  createInitialsForEachName(contactsFromApi);
+  sortAllContactsByFirstLetter();
+  renderContactsHeaderLetter();
 }
-
 
 /**
  * Get all the fetched contacts from the API and puts them in an array specified by the first letter.
- * 
+ *
  */
-function sortAllContactsByFirstLetter(){
-    sortedContactsArrayByFirstLetter = contactsFromApi.reduce((newArray, element) => {
-        const firstLetter = element.name.slice(0, 1).toUpperCase();
-              
-            return {...newArray,
-                [firstLetter] : [...(newArray[firstLetter] || []), element]
-            };
-        },
-            {});
-}
+function sortAllContactsByFirstLetter() {
+  sortedContactsArrayByFirstLetter = contactsFromApi.reduce(
+    (newArray, element) => {
+      const firstLetter = element.name.slice(0, 1).toUpperCase();
 
+      return {
+        ...newArray,
+        [firstLetter]: [...(newArray[firstLetter] || []), element],
+      };
+    },
+    {}
+  );
+}
 
 /**
  * Get the keys from an array, sort and render the first letter to the html content - contacts.
- * 
+ *
  */
-function renderContactsHeaderLetter(){
-    let firstLetterArray = Object.keys(sortedContactsArrayByFirstLetter);
-        firstLetterArray.sort();
-    const contentRef = document.getElementById('contacts-content');
-          contentRef.innerHTML = '';
-          firstLetterArray.forEach(firstLetterArray => {
-            contentRef.innerHTML += firstLetterContainerTemp(firstLetterArray);
-          })
+function renderContactsHeaderLetter() {
+  let firstLetterArray = Object.keys(sortedContactsArrayByFirstLetter);
+  firstLetterArray.sort();
+  const contentRef = document.getElementById("contacts-content");
+  contentRef.innerHTML = "";
+  firstLetterArray.forEach((firstLetterArray) => {
+    contentRef.innerHTML += firstLetterContainerTemp(firstLetterArray);
+  });
 }
-
 
 /**
  * Iterate through the contact list and render every single contact to the contact list to the html site contacts.
- * 
+ *
  * @param {string} firstLetterArray - The first letter to select the right array to render the names and set the background color to the initials.
  * @returns - Returns the whole contats sorted by specified letter.
  */
-function getSingleContact(firstLetterArray){
-    let content = '';
-    sortedContactsArrayByFirstLetter[firstLetterArray].forEach(nameRow => {
-                content += getSingleContactTemp(nameRow, firstLetterArray);
-    })
-    return content;
+function getSingleContact(firstLetterArray) {
+  let content = "";
+  sortedContactsArrayByFirstLetter[firstLetterArray].forEach((nameRow) => {
+    content += getSingleContactTemp(nameRow, firstLetterArray);
+  });
+  return content;
 }
-
 
 /**
  * Render the selected contact name in the floating contacts menu.
- * 
+ *
  * @param {string} contactId - The exact id in the users name object to get the informations.
  * @param {string} colorLetter - Headerletter to set the backgroundcolor css class to initials.
  */
-function openContactInFloatMenu(contactId, colorLetter){
-    const contentRef = document.getElementById('bottom-board');
-    const contact = contactsFromApi.find(element => element.id === contactId); 
-          contentRef.innerHTML = getSingleContactForFloatingMenuTemp(contact, colorLetter);
-        animateContactMenu();
-        addBackgroundToSelectedContact(contactId);
+function openContactInFloatMenu(contactId, colorLetter) {
+  const contentRef = document.getElementById("bottom-board");
+  const contact = contactsFromApi.find((element) => element.id === contactId);
+  contentRef.innerHTML = getSingleContactForFloatingMenuTemp(
+    contact,
+    colorLetter
+  );
+  animateContactMenu();
+  addBackgroundToSelectedContact(contactId);
 }
 
 /**
  * Animate the contact details to slide from the right outside under the contacts header.
- * 
+ *
  */
-function animateContactMenu(){
- const menuRef = document.getElementById('bottom-board');
-          requestAnimationFrame(() => {
-            menuRef.classList.add('floating-contact-container-open');
-          });
+function animateContactMenu() {
+  const menuRef = document.getElementById("bottom-board");
+  requestAnimationFrame(() => {
+    menuRef.classList.add("floating-contact-container-open");
+  });
 }
 
 /**
  * Check if the contact already exists in the database. Shows a red border and a text message if yes.
  * Fetches the new contact to the database and shows it in the floating menu.
- * 
+ *
  * @param {object} event - Default object to prevent the form to refresh the page.
  */
-async function createNewContact(event){
-    event.preventDefault();
-    await getNewContactTemp();
-    collectFormInformation('new-contact-form');
-    if (await checkIfDataAlreadyExists('user-email-input', 'contacts')) {
-        addRedBorderAndTextFalseInput('user-email-input', 'input-alert-message', 'Contact/Email already exists!');
-        setTimeout(() => removeRedBorderAndTextFalseInput('user-email-input', 'input-alert-message'), 3000);
-    } else {
-    await postDataToApi('contacts', collectedFormInfos);
-    toggleOverlayMenu('add-contact-overlay', 'add-contact-mask-container');
+async function createNewContact(event) {
+  event.preventDefault();
+  await getNewContactTemp();
+  collectFormInformation("new-contact-form");
+  if (await checkIfDataAlreadyExists("user-email-input", "contacts")) {
+    addRedBorderAndTextFalseInput(
+      "user-email-input",
+      "input-alert-message",
+      "Contact/Email already exists!"
+    );
+    setTimeout(
+      () =>
+        removeRedBorderAndTextFalseInput(
+          "user-email-input",
+          "input-alert-message"
+        ),
+      3000
+    );
+  } else {
+    await postDataToApi("contacts", collectedFormInfos);
+    toggleOverlayMenu("add-contact-overlay", "add-contact-mask-container");
     await sortAndRenderContacts();
-    scrollToNewContact(`contact-${collectedFormInfos.id}`)
-    openContactInFloatMenu(`${collectedFormInfos.id}`, `${collectedFormInfos.name.slice(0, 1)}`, );
+    scrollToNewContact(`contact-${collectedFormInfos.id}`);
+    openContactInFloatMenu(
+      `${collectedFormInfos.id}`,
+      `${collectedFormInfos.name.slice(0, 1)}`
+    );
     showContactAddedSuccessButton();
-    document.getElementById('new-contact-form').reset();
-    }
-}   
+    document.getElementById("new-contact-form").reset();
+  }
+}
 
 /**
  * Fills the empty object collectedFormInfos with am template and the next id number from the database.
  */
-async function getNewContactTemp(){
-    collectedFormInfos =     {
-        "id": `${await getTheNextFreeIdNumberFromApi('contacts')}`,
-        "name": "",
-        "email": "",
-        "phone": ""
-      }
+async function getNewContactTemp() {
+  collectedFormInfos = {
+    id: `${await getTheNextFreeIdNumberFromApi("contacts")}`,
+    name: "",
+    email: "",
+    phone: "",
+  };
 }
 
 /**
  * After fetching the new contact data to the database, highlights the new fetched contact in the contact list.
- * 
+ *
  * @param {string} containerId - The id off the specified contact in the contact list.
  */
-function addBackgroundToSelectedContact(containerId){
-    const otherRef = document.querySelectorAll('.single-contact');
-    const ref = document.getElementById(`contact-${containerId}`);
-          otherRef.forEach(element => {
-            element.classList.remove('highlight-contact');
-          })
-          ref.classList.add('highlight-contact');
+function addBackgroundToSelectedContact(containerId) {
+  const otherRef = document.querySelectorAll(".single-contact");
+  const ref = document.getElementById(`contact-${containerId}`);
+  otherRef.forEach((element) => {
+    element.classList.remove("highlight-contact");
+  });
+  ref.classList.add("highlight-contact");
 }
 
 /**
  * Displays a short overlay message in a button, if the contact successfully added to the database.
  */
-function showContactAddedSuccessButton(){
-    const ref = document.getElementById('contact-created-button');
-          setTimeout(() => ref.classList.add('contact-created-button-show'), 400);
-          setTimeout(() => ref.classList.remove('contact-created-button-show'), 2000);
+function showContactAddedSuccessButton() {
+  const ref = document.getElementById("contact-created-button");
+  setTimeout(() => ref.classList.add("contact-created-button-show"), 400);
+  setTimeout(() => ref.classList.remove("contact-created-button-show"), 2000);
 }
 
-
-function scrollToNewContact(contactId){
-    document.getElementById(contactId).scrollIntoView({behavior: "smooth", block: "end"});
+/**
+ * Scrolls to the selected contact.
+ * 
+ * @param {string} contactId - The id of the selected contact.
+ */
+function scrollToNewContact(contactId) {
+  document
+    .getElementById(contactId)
+    .scrollIntoView({ behavior: "smooth", block: "end" });
 }
 
-
-
-function openEditContact(contactId){
-    getInfosForEditMenu(contactId);
-    toggleOverlayMenu('edit-contact-overlay', 'edit-contact-mask-container');
+/**
+ * Opens the edit menu and fills it with information about the clicked user.
+ * 
+ * @param {string} contactId - The id of the contact to edit.
+ */
+function openEditContact(contactId) {
+  getInfosForEditMenu(contactId);
+  setOnclickEditAndDeleteToButtons(contactId);
+  toggleOverlayMenu("edit-contact-overlay", "edit-contact-mask-container");
 }
 
-function getInfosForEditMenu(contactId){
-    const contact = contactsFromApi.find(element => element.id === contactId);
-    document.getElementById('edit-user-name-input').value = contact.name;
-    document.getElementById('edit-user-email-input').value = contact.email;
-    document.getElementById('edit-user-phone-input').value = contact.phone;
-    document.getElementById('save-contact-button').setAttribute('onclick', `saveEditedContact(event, '${contactId}')`);
-    document.getElementById('delete-contact-button').setAttribute('onclick', `deleteContact('${contactId}')`);
+/**
+ * Collects all needed information to edit the contact.
+ * 
+ * @param {string} contactId - The id of the contact to edit.
+ */
+function getInfosForEditMenu(contactId) {
+  const contact = contactsFromApi.find((element) => element.id === contactId);
+  document.getElementById("edit-user-name-input").value = contact.name;
+  document.getElementById("edit-user-email-input").value = contact.email;
+  document.getElementById("edit-user-phone-input").value = contact.phone;
+
 }
 
-async function saveEditedContact(event, contactId){
-    event.preventDefault();
-    let contactKey = await getContactKeyFromApi(contactId);
-        await saveContactToApi(contactKey);
-        await sortAndRenderContacts();
-    let editedContact = contactsFromApi.find(element => element.id === contactId);
-        toggleOverlayMenu('edit-contact-overlay', 'edit-contact-mask-container');
-        scrollToNewContact(`contact-${contactId}`)
-        openContactInFloatMenu(contactId, editedContact.name.charAt(0).toUpperCase());
+/**
+ * Set the onclick functions to the buttons with specified parameters.
+ * 
+ * @param {string} contactId - The id of the contact parameter.
+ */
+function setOnclickEditAndDeleteToButtons(contactId){
+    document
+    .getElementById("save-contact-button")
+    .setAttribute("onclick", `saveEditedContact(event, '${contactId}')`);
+  document
+    .getElementById("delete-contact-button")
+    .setAttribute("onclick", `deleteContact('${contactId}')`);
 }
 
-async function deleteContact(contactId){
-    let contactKey = await getContactKeyFromApi(contactId);
-    await deleteContactFromApi(contactKey);
-    await sortAndRenderContacts();
-    if (!document.getElementById('edit-contact-overlay').classList.contains('standard-overlay-hide')) {
-        toggleOverlayMenu('edit-contact-overlay', 'edit-contact-mask-container')
-    };
-    emptyFloatMenu();
+/**
+ * Save the edited contact and animate the menu.
+ * 
+ * @param {*} event - For prevent the default function from the button.
+ * @param {string} contactId - The id of the contact to save.
+ */
+async function saveEditedContact(event, contactId) {
+  event.preventDefault();
+  let contactKey = await getContactKeyFromApi(contactId);
+  await saveContactToApi(contactKey);
+  await sortAndRenderContacts();
+  let editedContact = contactsFromApi.find(
+    (element) => element.id === contactId
+  );
+  toggleOverlayMenu("edit-contact-overlay", "edit-contact-mask-container");
+  scrollToNewContact(`contact-${contactId}`);
+  openContactInFloatMenu(contactId, editedContact.name.charAt(0).toUpperCase());
 }
 
-    async function getContactKeyFromApi(contactId){
-        let contactKey = '';        try {
-            let response = await fetch (MAIN_URL + "contacts.json");
-            if (!response.ok) {
-                throw new Error ("No answer from server");
-            } else {
-                let data = await response.json();
-                const contactNameObject = Object.entries(data).find(([key, element]) => {
-                    if (element !== null) {
-                        return element.id == contactId }});
-                contactKey = contactNameObject[0];
-            }
-        } catch(error) {
-            console.log(error);
+/**
+ * Delete the selected contact from the database and render new.
+ * 
+ * @param {string} contactId - The id of the selected contact.
+ */
+async function deleteContact(contactId) {
+  let contactKey = await getContactKeyFromApi(contactId);
+  await deleteContactFromApi(contactKey);
+  await sortAndRenderContacts();
+  if (
+    !document
+      .getElementById("edit-contact-overlay")
+      .classList.contains("standard-overlay-hide")
+  ) {
+    toggleOverlayMenu("edit-contact-overlay", "edit-contact-mask-container");
+  }
+  emptyFloatMenu();
+}
+
+/**
+ * Returns the key value as a string, from the specified contact id.
+ * 
+ * @param {string} contactId - The contact id of the selected contact.
+ * @returns - Returns the key from the contact in the database as a string.
+ */
+async function getContactKeyFromApi(contactId) {
+  let contactKey = "";
+  try {
+    let response = await fetch(MAIN_URL + "contacts.json");
+    if (!response.ok) {
+      throw new Error("No answer from server");
+    } else {
+      let data = await response.json();
+      const contactNameObject = Object.entries(data).find(([key, element]) => {
+        if (element !== null) {
+          return element.id == contactId;
         }
-        return contactKey;
+      });
+      contactKey = contactNameObject[0];
     }
+  } catch (error) {
+    console.log(error);
+  }
+  return contactKey;
+}
 
-
-    async function saveContactToApi(contactKey){
-        let contactName = document.getElementById('edit-user-name-input').value;
-        let contactMail = document.getElementById('edit-user-email-input').value;
-        let contactPhone = document.getElementById('edit-user-phone-input').value;
-        if (contactKey != undefined) {
-        try {
-            let response = await fetch(MAIN_URL + "contacts/" + contactKey + ".json", {
-                method: "PATCH",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                    name: contactName,
-                    email: contactMail,
-                    phone: contactPhone,
-                }),
-            })
-            if (!response.ok) {
-                throw new Error('Contact not found in Database!')
-            } else {
-                console.log(response);
-            }
-        } catch(error) {
-            console.log(error);
+/**
+ * Get the new information from the input fields and patch them to the database.
+ * 
+ * @param {string} contactKey - The keyvalue for the database to patch the payload.
+ */
+async function saveContactToApi(contactKey) {
+  let contactName = document.getElementById("edit-user-name-input").value;
+  let contactMail = document.getElementById("edit-user-email-input").value;
+  let contactPhone = document.getElementById("edit-user-phone-input").value;
+  if (contactKey != undefined) {
+    try {
+      let response = await fetch(
+        MAIN_URL + "contacts/" + contactKey + ".json",
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: contactName,
+            email: contactMail,
+            phone: contactPhone,
+          }),
         }
-        }}
+      );
+      if (!response.ok) {
+        throw new Error("Contact not found in Database!");
+      } else {
+        console.log(response);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 
+/**
+ * 
+ * @param {string} contactKey - The keyvalue to delete the user in the database.
+ */
+async function deleteContactFromApi(contactKey) {
+  if (contactKey != "") {
+    try {
+      await fetch(MAIN_URL + "contacts/" + contactKey + ".json", {
+        method: "DELETE",
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  } else {
+    console.log("contactKey is empty!");
+  }
+}
 
-        async function deleteContactFromApi(contactKey){
-            if (contactKey != "") {
-            try {
-                await fetch (MAIN_URL + "contacts/" + contactKey + ".json", {
-                    method: "DELETE",
-                });
-            } catch (error) {
-                console.log(error);
-            }
-        } else {
-            console.log("contactKey is empty!");
-        }}
-
-
-        function emptyFloatMenu(){
-            const menuRef = document.getElementById('bottom-board');
-                  menuRef.classList.remove('floating-contact-container-open');
-        }
+/**
+ * Close the deleted contact in the float menu.
+ */
+function emptyFloatMenu() {
+  const menuRef = document.getElementById("bottom-board");
+  menuRef.classList.remove("floating-contact-container-open");
+}
