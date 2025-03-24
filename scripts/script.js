8cfb35469f8e3eee88c03e50be991d1528da3c80
Alex <127949591+inkog1nto@users.noreply.github.com>
2025-03-24 19:16:03 +0100

    raw script for assignd to done // addtask

--- scripts/script.js
+++ scripts/script.js
@@ -336,7 +336,6 @@ function renderDropdownWithSearchResults(filteredUsers) {
     });
 }
 
-
 function handleCheckboxChange(event) {
     const userEmail = event.target.getAttribute("data-user-id");
     const user = usersFromApi.find(u => u.email === userEmail); 
@@ -345,20 +344,21 @@ function handleCheckboxChange(event) {
         const selectedDiv = document.createElement("div");
         selectedDiv.classList.add("selected-contact");
         selectedDiv.innerHTML = `
-            <div class="initials-circle">${getInitialsForObject(user)}</div>
-            <span>${user.name}</span>
-            <input type="checkbox" data-user-id="${userEmail}" class="user-checkbox" checked onclick="handleCheckboxChange(event)">
+            <div class="initials-circle">${getInitialsForObjectContacts(user)}</div>
         `;
         addSelectedContactsDiv.appendChild(selectedDiv);
+
     } else {
         const selectedDivs = addSelectedContactsDiv.querySelectorAll(".selected-contact");
         selectedDivs.forEach(div => {
-            if (div.querySelector("input").getAttribute("data-user-id") === userEmail) {
+            let divCheckbox = div.querySelector("input");
+            if (divCheckbox.getAttribute("data-user-id") === userEmail) {
                 div.remove();
             }
         });
     }
-}
+  }
+  
 
 function userLoggedIn() {
   let userNavbarREF = document.getElementById("user-navbar");
