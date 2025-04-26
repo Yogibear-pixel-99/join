a10b55b9ffd871da1ebd6af3df346f8c2f2e08ad
Yogibear-pixel-99 <joachim.puercher@gmail.com>
2025-04-26 22:14:43 +0200

    finished join v 0.2

--- scripts/contacts.js
+++ scripts/contacts.js
@@ -193,7 +193,7 @@ function emptyFormInputFields(id) {
  * @param {string} formEmailId - The id of the input container
  * @param {string} formPhoneId - The id of the input container
  * @param {string} errorId - The id of the error container
- * @returns
+ * @returns - A boolean.
  */
 function checkContactFormValidation(formNameId, formEmailId, formPhoneId, errorId) {
   let nameRef = document.getElementById(formNameId);
@@ -306,9 +306,7 @@ function showContactAddedSuccessButton() {
  * @param {string} contactId - The id of the selected contact.
  */
 function scrollToNewContact(contactId) {
-  document
-    .getElementById(contactId)
-    .scrollIntoView({ behavior: "smooth", block: "end" });
+  document.getElementById(contactId).scrollIntoView({ behavior: "smooth", block: "end" });
 }
 
 
@@ -346,12 +344,8 @@ function getInfosForEditMenu(contactKey) {
  * @param {string} contactKey - The key of the contact parameter.
  */
 function setOnclickEditAndDeleteToButtons(contactKey) {
-  document
-    .getElementById("save-contact-button")
-    .setAttribute("onclick", `saveEditedContact(event, '${contactKey}')`);
-  document
-    .getElementById("delete-contact-button")
-    .setAttribute("onclick", `deleteContact('${contactKey}')`);
+  document.getElementById("save-contact-button").setAttribute("onclick", `saveEditedContact(event, '${contactKey}')`);
+  document.getElementById("delete-contact-button").setAttribute("onclick", `deleteContact('${contactKey}')`);
 }
 
 
@@ -442,20 +436,6 @@ function toggleEditDeleteContactMenuMobile() {
 
 
 /**
- * Toggles classes to toggle the edit/delete menu in floatin contacts menu.
- *
- * @param {HTMLElement} menuRef - The menu element.
- * @param {HTMLElement} maskRef - The mask element.
- * @param {HTMLElement} mainRef - The main element.
- */
-function toggleClasses(menuRef, maskRef, mainRef) {
-  menuRef.classList.toggle("show-floating-edit-delete-wrapper-mobile");
-  maskRef.classList.toggle("d-none");
-  mainRef.classList.toggle("disable-pointer-events");
-}
-
-
-/**
  * Set new initials to the container if the name is changing.
  *
  * @param {string} inputId - The name value from the input field.
@@ -467,26 +447,6 @@ function setInitialsToNewContactContainer(inputId, destinationId) {
   let destinationRef = document.getElementById(destinationId);
   destinationRef.innerText = initials.slice(0, 2);
   removeAndSetInititalsBackgroundColorClass(destinationId, initials);
-  destinationRef.classList.add(
-    `initials-bg-color-${initials.charAt(0).toUpperCase()}`
+  destinationRef.classList.add(`initials-bg-color-${initials.charAt(0).toUpperCase()}`
   );
-}
-
-
-/**
- * Removes the old background color class from the container and sets an empty placeholder icon.
- *
- * @param {string} initials - The initails form the input name value.
- * @param {HTMLElement} destinationRef - The destination container for the icon.
- */
-function removeAndSetInititalsBackgroundColorClass(destinationId, initials) {
-  let contentRef = document.getElementById(destinationId);
-  if (initials == "") {
-    contentRef.className = "";
-    contentRef.classList.add("add-contact-overlay-img-wrapper");
-    contentRef.classList.add("flex-ctr-ctr");
-    contentRef.innerHTML = `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M8 8C6.9 8 5.95833 7.60833 5.175 6.825C4.39167 6.04167 4 5.1 4 4C4 2.9 4.39167 1.95833 5.175 1.175C5.95833 0.391667 6.9 0 8 0C9.1 0 10.0417 0.391667 10.825 1.175C11.6083 1.95833 12 2.9 12 4C12 5.1 11.6083 6.04167 10.825 6.825C10.0417 7.60833 9.1 8 8 8ZM14 16H2C1.45 16 0.979167 15.8042 0.5875 15.4125C0.195833 15.0208 0 14.55 0 14V13.2C0 12.6333 0.145833 12.1125 0.4375 11.6375C0.729167 11.1625 1.11667 10.8 1.6 10.55C2.63333 10.0333 3.68333 9.64583 4.75 9.3875C5.81667 9.12917 6.9 9 8 9C9.1 9 10.1833 9.12917 11.25 9.3875C12.3167 9.64583 13.3667 10.0333 14.4 10.55C14.8833 10.8 15.2708 11.1625 15.5625 11.6375C15.8542 12.1125 16 12.6333 16 13.2V14C16 14.55 15.8042 15.0208 15.4125 15.4125C15.0208 15.8042 14.55 16 14 16ZM2 14H14V13.2C14 13.0167 13.9542 12.85 13.8625 12.7C13.7708 12.55 13.65 12.4333 13.5 12.35C12.6 11.9 11.6917 11.5625 10.775 11.3375C9.85833 11.1125 8.93333 11 8 11C7.06667 11 6.14167 11.1125 5.225 11.3375C4.30833 11.5625 3.4 11.9 2.5 12.35C2.35 12.4333 2.22917 12.55 2.1375 12.7C2.04583 12.85 2 13.0167 2 13.2V14ZM8 6C8.55 6 9.02083 5.80417 9.4125 5.4125C9.80417 5.02083 10 4.55 10 4C10 3.45 9.80417 2.97917 9.4125 2.5875C9.02083 2.19583 8.55 2 8 2C7.45 2 6.97917 2.19583 6.5875 2.5875C6.19583 2.97917 6 3.45 6 4C6 4.55 6.19583 5.02083 6.5875 5.4125C6.97917 5.80417 7.45 6 8 6Z" fill="#A8A8A8"/>
-                  </svg> `;
-  }
 }
