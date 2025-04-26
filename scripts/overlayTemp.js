22272780e46abc4069598c56594f07c534548fe3
EmreIscan-47 <iscan.connect@outlook.de>
2025-04-26 14:08:58 +0200

    resetAddTaskForm added

--- scripts/overlayTemp.js
+++ scripts/overlayTemp.js
@@ -283,7 +283,7 @@ function getAddTaskOverlayTemp(){
               <span>This field is required</span>
             </div>
             <div class="addTask-button-container">
-              <div onclick="resetForm('add-task-form'); setPriorityButtonColor(document.getElementById('standard-prio')); deleteSubtasksContent();" class="light-button btns-addtask flex-ctr-ctr button-wrapper-add-task">
+              <div onclick="resetForm('add-task-form'); resetAddTaskForm(); setPriorityButtonColor(document.getElementById('standard-prio')); deleteSubtasksContent();" class="light-button btns-addtask flex-ctr-ctr button-wrapper-add-task">
                 <div
                   class="">
                   Clear
